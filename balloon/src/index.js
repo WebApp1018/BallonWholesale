d724c3457152448dfc8dede039491cc102cba6e6
ZhengCheng Hong <122842048+zhengchenghong@users.noreply.github.com>
2023-05-11 03:47:12 +0900

    init base structure

--- balloon/src/index.js
+++ balloon/src/index.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Route, Routes, BrowserRouter as Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { store } from './app/store';
 import './index.css';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
@@ -7,9 +10,13 @@ import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Provider store={store}>
+    <Router>
+      <Routes>
+        <Route path='/*' element={<App />} />
+      </Routes>
+    </Router>
+  </Provider>
 );
 
 // If you want your app to work offline and load faster, you can change
