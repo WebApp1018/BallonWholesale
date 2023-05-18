30c86b33605eae832f83f38595a3484a8011cfba
George Lucas <zhengcheng.hong1018@gmail.com>
2023-05-18 23:42:01 +0900

    keys set up

--- config/keys_dev.js
+++ config/keys_dev.js
@@ -1,4 +1,4 @@
 module.exports = {
-  MONGODB_URI: process.env.MONGODB_URI,
+  MONGODB_URI: "mongodb+srv://daringwebdesigner:QsHA1Jq4oBdw6eT3@cluster0.owswhbo.mongodb.net/?retryWrites=true&w=majority",
   JWT_SECRET: "secret"
 };
