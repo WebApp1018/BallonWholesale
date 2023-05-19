3a2d77272eb44b1226113f0d5f17e9650e992c20
George Lucas <zhengcheng.hong1018@gmail.com>
2023-05-19 09:17:14 +0900

    /public/upload to public/upload

--- routes/api/product.js
+++ routes/api/product.js
@@ -17,7 +17,7 @@ const validateAddProductInput = require("../../validation/validateAddProductInpu
 const multer = require('multer');
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, '/public/upload')
+    cb(null, 'public/upload')
   },
   filename: function (req, file, cb) {
     cb(null, file.originalname)
