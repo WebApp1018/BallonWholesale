89149f8e7be1926f3514d6b4e65b2b02354ee487
George Lucas <zhengcheng.hong1018@gmail.com>
2023-06-09 11:29:18 +0900

    80 port

--- index.js
+++ index.js
@@ -1,49 +1,49 @@
-const express = require("express");
-const passport = require("passport");
-const mongoose = require("mongoose");
-const bodyParser = require("body-parser");
-const configVars = require("./config/keys");
-const path = require("path");
-var cors = require("cors");
+// const express = require("express");
+// const passport = require("passport");
+// const mongoose = require("mongoose");
+// const bodyParser = require("body-parser");
+// const configVars = require("./config/keys");
+// const path = require("path");
+// var cors = require("cors");
 
 // Import routes
-const auth = require("./routes/api/auth");
-const product = require("./routes/api/product");
+// const auth = require("./routes/api/auth");
+// const product = require("./routes/api/product");
 
-const port = process.env.PORT || 5000;
-const app = express();
+// const port = process.env.PORT || 5000;
+// const app = express();
 
 // General middlewares
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-app.use(passport.initialize());
-require("./config/passport")(passport);
-app.use(cors());
-app.options("*", cors());
-app.use("/public", express.static("public"));
+// app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(bodyParser.json());
+// app.use(passport.initialize());
+// require("./config/passport")(passport);
+// app.use(cors());
+// app.options("*", cors());
+// app.use("/public", express.static("public"));
 
 // Connect to MongoDB
-mongoose
-  .connect(configVars.MONGODB_URI, { useNewUrlParser: true })
-  .then(() => console.log("MongoDB connected!"))
-  .catch((err) => console.log(err));
+// mongoose
+//   .connect(configVars.MONGODB_URI, { useNewUrlParser: true })
+//   .then(() => console.log("MongoDB connected!"))
+//   .catch((err) => console.log(err));
 
 // Use routes
-app.use("/api/auth", auth);
-app.use("/api/product", product);
+// app.use("/api/auth", auth);
+// app.use("/api/product", product);
 
 // Serve static files from the 'build' folder
-app.use(express.static(path.join(__dirname, "balloon/build")));
+// app.use(express.static(path.join(__dirname, "balloon/build")));
 
 // Handle other requests by serving the index.html file
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "balloon/build/index.html"));
-});
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(__dirname, "balloon/build/index.html"));
+// });
 
 // Start the server
-app.listen(port, () => {
-  console.log(`Balloon app listening at http://149.100.159.216:${port}`);
-});
+// app.listen(port, () => {
+//   console.log(`Balloon app listening at http://149.100.159.216:${port}`);
+// });
 
 // RUN AS HTTPS SERVER ------------------
 // var fs = require('fs')
@@ -53,3 +53,14 @@ app.listen(port, () => {
 // var credentials = {key: privateKey, cert: certificate};
 // var httpsServer = https.createServer(credentials, app);
 // httpsServer.listen(443);
+
+const express = require("express");
+
+// Configure & Run the http server
+const app = express();
+
+app.use(express.static(__dirname, { dotfiles: "allow" }));
+
+app.listen(80, () => {
+  console.log("HTTP server running on port 80");
+});
