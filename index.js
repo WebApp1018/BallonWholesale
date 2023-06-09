f7a937757d6817270314b58e4d2e451475416aa8
George Lucas <zhengcheng.hong1018@gmail.com>
2023-06-09 11:58:47 +0900

    5000 port

--- index.js
+++ index.js
@@ -61,6 +61,6 @@ const app = express();
 
 app.use(express.static(__dirname, { dotfiles: "allow" }));
 
-app.listen(80, () => {
-  console.log("HTTP server running on port 80");
+app.listen(5000, () => {
+  console.log("HTTP server running on port 5000");
 });
